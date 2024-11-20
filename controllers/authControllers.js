4fdc65e0edbb6a7e00b4314344eeeb0793ea1b33
praveen piramanayagam <praveennayagam18@gmail.com>
2024-11-20 13:23:32 +0530

    till register controller

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,8 +1,34 @@
+const User = require("../models/user");
+
 const authController = {
     register: async(request,response)=>{
-        console.log("Registered");
+    try {
+        //extract details name,email,password
+        const {name,email,password} = request.body;
+
+        //check of email exists
+        const userExists = await User.findOne({email:email});
+
+        //if user exists return error message
+        if (userExists) {
+            return response.status(400).json({error:"user already exists"});
+        }
+         //create new object
+         const newUser = new User({
+            name,
+            email,
+            password    
+        });
+
+        //save to db
+        await newUser.save();
+        return response.status(201).json({message:'User created successfully'});
         
+    } catch (error) {
+        return response.status(500).json({error: error.message});
+    }        
     },
+
     login: async(request,response)=>{
         console.log("login");
         
--- models/user.js
+++ models/user.js
@@ -4,20 +4,7 @@ const { type } = require('os');
 const userSchema = new mongoose.Schema({
     "name" : String,
     "email" : String,
-    "password" : String,
-    "role" : {
-        type : String,
-        enum : ['Admin','User','Manager'],
-        default : 'user'
-    },
-    "createdAt" : {
-        type : Date,
-        default : Date.now
-    },
-    "updatedAt" : {
-        type : Date,
-        default : Date.now
-    }  
+    "password" : String, 
 });
-
-module.exports = mongoose.model('User',userSchema,'users');
+const User = mongoose.model('User',userSchema);
+module.exports = User;
